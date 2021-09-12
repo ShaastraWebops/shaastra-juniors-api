4732dd7f76b33b1389de22062553538d0d4c7762
JanithMS <janithms9920@gmail.com>
2021-09-12 13:28:54 +0530

    mails

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,37 +1,19 @@
 import nodemailer from "nodemailer";
-import { google } from "googleapis";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-
-const oAuth2Client = new google.auth.OAuth2(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REDIRECT_URI
-);
-
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN});
-
 export const mail = async ({ email, sub, body } : { email: string, sub: string, body: string }) => {
     const sendMail = async () => {
         try {
-            const accessToken = await oAuth2Client.getAccessToken();
     
             const transport = nodemailer.createTransport({
-                service: "gmail",
+                host: "smtp-relay.sendinblue.com",
+                port: 587,
                 auth: {
-                    type: 'OAuth2',
                     user: 'webops@shaastra.org',
-                    clientId: CLIENT_ID,
-                    clientSecret: CLIENT_SECRET,
-                    refreshToken: REFRESH_TOKEN,
-                    accessToken: accessToken,
+                    pass: process.env.MAIL_PASS
                 },
             }  as SMTPTransport.Options);
     
